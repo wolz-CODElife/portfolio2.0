82a95a2e8e65b356c00973b5e7ca6f321827760b
Joel Adewole <joeladewole3@gmail.com>
2023-11-20 11:40:50 +0100

    Pushed new portfolio version 🚀

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import { IconGithub, IconGmail, IconHashnode, IconLinkedin, IconTwitter, IconYou
 
 const Footer = () => {const socials = [
     {
-        img: <IconHashnode  color="#a0b49c"/>,
+        img: <IconHashnode width='100%' height='100%' color="#a0b49c"/>,
         link: "https://blog.wolzcodelife.com",
         title: "Blog"
     },
